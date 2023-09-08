9dafd7fa43c89cccc5e4b8417705ab3d27431896
Romanets Roman <76920196+Romanuch07@users.noreply.github.com>
2023-09-08 23:11:47 +0200

    zad5/dots
    add dots

--- Assets/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/Scripts/Gameplay/Player/PlayerController.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public static PlayerController Instance;
+    // private PlayerInput playerInput;
 
     [SerializeField] private FloatVariable HealthVariable;
     [SerializeField] private FloatVariable ManaVariable;
@@ -23,23 +25,37 @@ public class PlayerController : MonoBehaviour
     [SerializeField] SkillSO JumpSkill;
     [SerializeField] SkillSO SprintSkill;
 
+    [Header("Input Actions References")]
+    [SerializeField] InputActionAsset playerInput;
+
     private float yMovement = -9.81f;
 
     private void Awake()
     {
         if (Instance != null)
             Debug.LogError("There is more than one instance of this!", gameObject);
-        
+
         Instance = this;
         characterController = GetComponent<CharacterController>();
-        Steamworks.SteamUserStats.SetAchievement("playerControlled");
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
     }
 
     private void Update()
     {
-        var movementValue = new Vector2(Input.GetAxis("Horizontal"),  Input.GetAxis("Vertical"));
 
-        if (SprintSkill.IsActive && Input.GetKey(KeyCode.LeftShift) && StaminaVariable.Value > 0)
+        // var movementValue = new Vector2(Input.GetAxis("Horizontal"),  Input.GetAxis("Vertical"));
+        var movementValue = playerInput.FindAction("Move").ReadValue<Vector2>();
+
+        if (SprintSkill.IsActive && playerInput.FindAction("Sprint").ReadValue<float>() > 0f && StaminaVariable.Value > 0)
         {
             movementValue *= sprintModificator;
             StaminaVariable.Value -= staminaUse * Time.deltaTime;
@@ -54,10 +70,10 @@ public class PlayerController : MonoBehaviour
         movementValue *= Time.deltaTime;
 
         characterController.Move(new Vector3(movementValue.x, yMovement * Time.deltaTime, movementValue.y));
-        if(characterController.velocity.sqrMagnitude > 0.1)
+        if (characterController.velocity.sqrMagnitude > 0.1)
             transform.forward = new Vector3(movementValue.x, 0f, movementValue.y);
 
-        if (JumpSkill.IsActive && Input.GetKeyDown(KeyCode.Space) && characterController.isGrounded)
+        if (JumpSkill.IsActive && playerInput.FindAction("Jump").ReadValue<float>() > 0f && characterController.isGrounded)
             yMovement = 10f;
 
         yMovement = Mathf.Max(-9.81f, yMovement - Time.deltaTime * 30f);

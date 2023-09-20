e8ba47dd55813527357debff79beda7e2521ad6b
Romanets Roman <76920196+Romanuch07@users.noreply.github.com>
2023-09-20 16:59:20 +0200

    Dots
    add scene dots

---
+++ Assets/_Dots/Scripts/ClosestAndLast.cs
@@ -0,0 +1,10 @@
+namespace Dots.JobsScripting
+{
+    public struct ClosestAndLast
+    {
+        public int first;
+        public int second;
+        public int third;
+        public int last;
+    }
+}

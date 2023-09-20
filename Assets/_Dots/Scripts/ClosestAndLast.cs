4831ad2ebb8211672bc037e3c7e04e7704d7ca28
Romanets Roman <76920196+Romanuch07@users.noreply.github.com>
2023-09-20 17:46:38 +0200

    fix bug on dots scripts
    fixed problem

--- Assets/_Dots/Scripts/ClosestAndLast.cs
+++ Assets/_Dots/Scripts/ClosestAndLast.cs
@@ -1,3 +1,11 @@
+using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Serialization;
+
 namespace Dots.JobsScripting
 {
     public struct ClosestAndLast
